a9a7dc41cada337f08aea3e4f63b24a1422c463a
Antoine Doolaeghe <antoine.doolaeghe@draysonhealth.com>
2019-08-04 10:27:44 +0200

    Update api key

--- Assets/Scripts/Api/ApiConfig.cs
+++ Assets/Scripts/Api/ApiConfig.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ApiConfig : ScriptableObject
 {
   public static string url = "https://sw2hs7ufb5gevarvuyswhrndjm.appsync-api.eu-west-1.amazonaws.com/graphql";
-  public static string apiKey = "da2-fmeunisjmbfnfdpnf6eiia7xvq";
+  public static string apiKey = "da2-5aipqbxwx5eyfkoain3jhxtigm";
   public static string updateTrainerData = "query updateTrainerData {updateTrainer(trainerKey: $key^, trainerData: $data^)}";
   public static string downloadTrainerData = "query update {downloadTrainer(trainerKey: $key^)}";
 }
